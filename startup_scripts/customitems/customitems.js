0e2de2c2a9c29049c6bf0dde0fb25ba55be6c76a
Nikolai Jakobslock <hallowfiend@protonmail.com>
2026-07-26 18:58:09 +0200

    embering it so good rn (

--- startup_scripts/customitems/customitems.js
+++ startup_scripts/customitems/customitems.js
@@ -10,4 +10,29 @@ StartupEvents.registry('item', event => {
     //hibernal herbs sigil replacements
     event.create('singed_leaves')
     .displayName('Singed Leaves');
+    //custom aspecti
+    const customAspectiMetals = [
+        'tin',
+        'zinc',
+        'aluminium',
+        'gold',
+        'rose_gold',
+        'sterling_silver',
+        'electrum',
+        'constantan',
+        'invar',
+        'pewter',
+        'bronze',
+        'brass',
+        'duralumin'
+    ]
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    customAspectiMetals.forEach(metal =>{
+        var metalLang = capitalizeFirstLetter(metal)
+        event.create(`aspectus_${metal}`)
+        .displayName(`${metalLang} Aspectus`)
+        .tag(`embers:aspectus/${metal}`);
+    })
 })
