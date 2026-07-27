1172c0c7441abec27e12892794ef27262183b6ef
Nikolai Jakobslock <hallowfiend@protonmail.com>
2026-07-27 11:10:32 +0200

    spirit focusing & alchemy progress

---
+++ server_scripts/recipes/magic/malum/spiritfocusing.js
@@ -0,0 +1,82 @@
+ServerEvents.recipes(event =>{
+    function pureSpiritFocusing(type, output, outputCount){
+        event.custom({
+        "type": "malum:spirit_focusing",
+        "durabilityCost": 1,
+        "input": {
+                "item": "malum:alchemical_impetus"
+            },
+        "output": {
+            "count": outputCount,
+            "item": output
+        },
+        "spirits": [
+            {
+            "type": type,
+            "count": 1
+            }
+        ],
+        "time": 300
+    })
+    }
+    function dualSpiritFocusing(type1, type2, output, outputCount){
+        event.custom({
+        "type": "malum:spirit_focusing",
+        "durabilityCost": 1,
+        "input": {
+                "item": "malum:alchemical_impetus"
+            },
+        "output": {
+            "count": outputCount,
+            "item": output
+        },
+        "spirits": [
+            {
+            "type": type1,
+            "count": count1
+            },
+            {
+            "type": type2,
+            "count": count2
+            }
+        ],
+        "time": 300
+    })
+    }
+    //pureSpiritFocusing("eldritch", 1, )
+    dualSpiritFocusing("eldritch", "arcane", "mna:vinteum_dust", 8)
+    dualSpiritFocusing("eldritch", "wicked", "tconstruct:ender_slime_crystal", 4)
+    dualSpiritFocusing("eldritch", "sacred", "eidolon:soul_shard", 1)
+    dualSpiritFocusing("eldritch", "aerial", "tconstruct:sky_slime_crystal", 4)
+    dualSpiritFocusing("eldritch", "earthen", "tconstruct:earth_slime_crystal", 4)
+    dualSpiritFocusing("eldritch", "infernal", "tconstruct:ichor_slime_crystal", 4)
+    dualSpiritFocusing("eldritch", "aqueous", "theurgy:mercury_shard", 8)
+    //pure arcane: redstone
+    //dualSpiritFocusing("arcane", "wicked", "")
+    //dualSpiritFocusing("arcane", "sacred", )
+    //arcane + aerial: amethyst
+    //arcane + infernal: blazing quartz
+    //arcane + earthen: quartz
+    //arcane + aqueous: prismarine
+    pureSpiritFocusing("wicked", "minecraft:soul_sand", 8)
+    //dualSpiritFocusing("wicked", "sacred",)
+    dualSpiritFocusing("wicked", "aerial", "goety:jade", 8)
+    dualSpiritFocusing("wicked", "infernal", "minecraft:blaze_powder", 8)
+    dualSpiritFocusing("wicked", "earthen", "gtceu:saltpeter_dust", 8)
+    dualSpiritFocusing("wicked", "aqueous", "netherexp:banshee_powder", 8)
+    pureSpiritFocusing("sacred", "minecraft:sugar", 8)
+    dualSpiritFocusing("sacred", "aerial", "hexalia:celestial_crystal", 4)
+    //dualSpiritFocusing("sacred", "infernal", "")
+    dualSpiritFocusing("sacred", "earthen", "hexerei:selenite_shard", 8)
+    dualSpiritFocusing("sacred", "aqueous", "gtceu:salt_dust", 8)
+    //pureSpiritFocusing("aerial", "")
+    dualSpiritFocusing("aerial", "infernal", "gtceu:sulfur_dust", 8)
+    dualSpiritFocusing("aerial", "earthen", "ae2:certus_quartz", 4)
+    //dualSpiritFocusing("aerial", "aqueous", "")
+    //pure infernal: glowstone
+    dualSpiritFocusing("infernal", "earthen", "embers:ash", 8)
+    //dualSpiritFocusing("infernal", "aqueous", "")
+    //pure earthen: gunpowder
+    //dualSpiritFocusing("earthen", "aqueous", "")
+    //dualSpiritFocusing("aqueous", "")
+})
--- server_scripts/recipes/tech/embers/alchemy.js
+++ server_scripts/recipes/tech/embers/alchemy.js
@@ -3,6 +3,19 @@ const lead = { "tag": "embers:aspectus/lead" }
 const copper = { "tag": "embers:aspectus/copper" }
 const silver = { "tag": "embers:aspectus/silver" }
 const dawnstone = { "tag": "embers:aspectus/dawnstone" }
+const tin = {"tag": "embers:aspectus/tin"}
+const zinc = {"tag": "embers:aspectus/tin"}
+const aluminium = {"tag": "embers:aspectus/aluminium"}
+const gold = {"tag": "embers:aspectus/gold"}
+const rose_gold = {"tag": "embers:aspectus/rose_gold"}
+const sterling_silver = {"tag": "embers:aspectus/sterling_silver"}
+const electrum = {"tag": "embers:aspectus/electrum"}
+const constantan = {"tag": "embers:aspectus/constantan"}
+const invar = {"tag": "embers:aspectus/invar"}
+const pewter = {"tag": "embers:aspectus/pewter"}
+const bronze = {"tag": "embers:aspectus/bronze"}
+const brass = {"tag": "embers:aspectus/brass"}
+const duralumin = {"tag": "embers:aspectus/duralumin"}
 
 ServerEvents.recipes(event => {
     event.remove({ output: "bloodmagic:lavacrystal" })
@@ -36,15 +49,203 @@ ServerEvents.recipes(event => {
     mixing("lead", 2, "iron", 2, "pewter", 4)
     mixing("copper", 2, "aluminium", 6, "duralumin", 8)
     //the aspecti proper
-    
+    const customAspectiMetals = [
+        'tin',
+        'zinc',
+        'aluminium',
+        'gold',
+        'rose_gold',
+        'sterling_silver',
+        'electrum',
+        'constantan',
+        'invar',
+        'pewter',
+        'bronze',
+        'brass',
+        'duralumin'
+    ]
+    customAspectiMetals.forEach(metal =>{
+        event.custom({
+            "type": "embers:stamping",
+            "fluid": {
+                "amount": 144,
+                "tag": `forge:${metal}`
+            },
+            "input": {
+                "item": "embers:ember_shard"
+            },
+            "output": {
+                "item": `kubejs:aspectus_${metal}`
+            },
+            "stamp": {
+                "item": "embers:ingot_stamp"
+        }
+        })
+    })
     
     //Convenience recipes
-
+    //TF MATERIAL DUPES
+    //Ironwood
+    event.custom({
+        "type": "embers:alchemy",
+        "aspects": [iron, invar],
+        "inputs": [
+            {
+                "item": "minecraft:iron_ingot"
+            },
+            {
+                "item": "malum:alchemical_calx"
+            },
+            {
+                "item": "malum:earthen_spirit"
+            },
+            {
+                "tag": "farmersdelight:barks"
+            }
+        ],
+        "output": {
+            "count": 4,
+            "item": "twilightforest:ironwood_ingot"
+        },
+        "tablet": {
+            "item": "twilightforest:ironwood_ingot"
+        }
+    })
+    //Steeleaf
+    event.custom({
+        "type": "embers:alchemy",
+        "aspects": [zinc, duralumin],
+        "inputs": [
+            {
+                "tag": "forge:ingots/steel"
+            },
+            {
+                "item": "eidolon:avennian_sprig"
+            },
+            {
+                "item": "malum:earthen_spirit"
+            },
+            {
+                "item": "farmersrespite:green_tea_leaves"
+            }
+        ],
+        "output": {
+            "count": 4,
+            "item": "twilightforest:steeleaf_ingot"
+        },
+        "tablet": {
+            "item": "twilightforest:steeleaf_ingot"
+        }
+    })
+    //Knightmetal
+    event.custom({
+        "type": "embers:alchemy",
+        "aspects": [zinc, invar, pewter],
+        "inputs": [
+            {
+                "item": "goety:cursed_ingot"
+            },
+            {
+                "item": "sons_of_sins:ether_ashes"
+            },
+            {
+                "item": "malum:earthen_spirit"
+            },
+            {
+                "tag": "forge:dusts/jade"
+            }
+        ],
+        "output": {
+            "count": 4,
+            "item": "twilightforest:knightmetal_ingot"
+        },
+        "tablet": {
+            "item": "twilightforest:knightmetal_ingot"
+        }
+    })
+    //Nagascale
+    event.custom({
+        "type": "embers:alchemy",
+        "aspects": [zinc, tin],
+        "inputs": [
+            {
+                "item": "alexsmobs:shed_snake_skin"
+            },
+            {
+                "item": "alexsmobs:crocodile_scute"
+            },
+            {
+                "item": "malum:grim_talc"
+            },
+            {
+                "tag": "forge:plates/zinc"
+            }
+        ],
+        "output": {
+            "count": 4,
+            "item": "twilightforest:naga_scale"
+        },
+        "tablet": {
+            "item": "twilightforest:naga_scale"
+        }
+    })
+    //Carminite
+    event.custom({
+        "type": "embers:alchemy",
+        "aspects": [iron, duralumin],
+        "inputs": [
+            {
+                "item": "hexerei:blood_bottle"
+            },
+            {
+                "item": "evilcraft:corrupted_tear"
+            },
+            {
+                "item": "malum:alchemical_calx"
+            },
+            {
+                "tag": "forge:gems/ruby"
+            }
+        ],
+        "output": {
+            "count": 8,
+            "item": "twilightforest:carminite"
+        },
+        "tablet": {
+            "item": "twilightforest:carminite"
+        }
+    })
+    //Aurora block
+    event.custom({
+        "type": "embers:alchemy",
+        "aspects": [rose_gold, sterling_silver],
+        "inputs": [
+            {
+                "item": "alexsmobs:rainbow_jelly"
+            },
+            {
+                "item": "malum:astral_weave"
+            },
+            {
+                "tag": "forge:glass"
+            },
+            {
+                "tag": "forge:glass"
+            }
+        ],
+        "output": {
+            "count": 16,
+            "item": "twilightforest:aurora_block"
+        },
+        "tablet": {
+            "item": "twilightforest:aurora_block"
+        }
+    })
     //Actual progression n shit
     //Lava crystal
     event.custom({
         "type": "embers:alchemy",
-        "aspects": [dawnstone, copper],
+        "aspects": [electrum, brass, dawnstone],
         "inputs": [
             {
                 "item": "bloodmagic:reagentlava"
@@ -67,7 +268,7 @@ ServerEvents.recipes(event => {
     //Depth ingots
     event.custom({
         "type": "embers:alchemy",
-        "aspects": [lead, iron, silver],
+        "aspects": [pewter, lead],
         "inputs": [
             {
                 "item": "naturesaura:tainted_gold"
